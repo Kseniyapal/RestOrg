9a9ca4ac351165e42e531856b0464162d9ede23a
kseniyapal <kseniyapal@yandex.ru>
2024-01-06 22:36:05 +0300

    add front

---
+++ restorg-react/src/Components/OrderItem.jsx
@@ -0,0 +1,24 @@
+import React, { Children, useState, useCallback, useEffect } from "react";
+import "../Components/ComponentsStyles/PurchaseItem.css"
+import PlusIco from "../Styles/icons/plus.svg"
+import Menu from "../Pages/Menu";
+
+const OrderItem = ({...props}) => {
+    const [buttonStyle, setButtonStyle] = useState({background: "#FFF"})
+
+    const changeColor = () => {
+        setButtonStyle({background: "#747474"})
+    }
+    return (
+        <div className="purchase__flex">    
+            <div className="purchase__img">
+                    <img src={"/" + props.imgSource}alt={props.imgSource}></img>
+            </div>
+            <div className="purchase__name order__name">{props.name}</div>
+            {/* <div  className="purchase__worked"><button onClick={changeColor} style={buttonStyle} >отработано</button></div> */}
+            
+    </div>                                
+    )
+}
+
+export default OrderItem;
