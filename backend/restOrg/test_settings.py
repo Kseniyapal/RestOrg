828a358848c3f131d5d8faca229dde2da8a9e28e
kseniyapal <kseniyapal@yandex.ru>
2024-01-07 23:43:14 +0300

    add fix testing

--- backend/restOrg/test_settings.py
+++ backend/restOrg/test_settings.py
@@ -98,16 +98,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'restOrg.wsgi.application'
 
 
-DATABASES = {
-     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "django"),
-        "USER": os.getenv("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "tvorogovo284"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", 5432)
-     }
-    
+DATABASES = {  
+    "default": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
